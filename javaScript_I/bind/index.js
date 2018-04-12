7c5d432395f640c25848ba6608232f40de0dac96
marisbotero <maris2143@gmail.com>
2018-04-12 16:08:53 -0500

    bind

---
+++ javaScript_I/bind/index.js
@@ -0,0 +1,24 @@
+
+class Toggable {
+    constructor(el) {
+      // inicializar el estado interno
+      this.el = el
+      this.el.innerHTML = 'Off'
+      this.activated = false
+      this.onClick = this.onClick.bind(this)
+      this.el.addEventListener('click', this.onClick)
+    }
+  
+    onClick(ev) {
+      this.activated = !this.activated
+      this.toggleText()
+    }
+  
+    toggleText() {
+      this.el.innerHTML = this.activated ? 'On' : 'Off'
+    }
+  }
+  
+  const button = document.getElementById('boton')
+  
+  const miBoton = new Toggable(button)
